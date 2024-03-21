1b44f9cef125007bed6c4946e853e8f2f64d574e
dengsb <dengsb@akulaku.com>
2024-03-21 14:27:08 +0800

    排除mongodb的自动加载

--- src/main/java/com/github/alenfive/rocketapi/RocketAPIApplication.java
+++ src/main/java/com/github/alenfive/rocketapi/RocketAPIApplication.java
@@ -3,8 +3,9 @@ package com.github.alenfive.rocketapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = MongoDataAutoConfiguration.class)
+@SpringBootApplication(exclude = {MongoDataAutoConfiguration.class, MongoAutoConfiguration.class})
 public class RocketAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(RocketAPIApplication.class, args);
